8a14fdc529684e5e95687973aaa1a06a659e5a12
ze-antunes <jprantunes2000@gmail.com>
2023-12-15 10:15:18 +0000

    update sword game

--- scripts/sword_game.js
+++ scripts/sword_game.js
@@ -76,8 +76,8 @@ function getIndividualLetters(str) {
 
         letterModel.innerHTML = htmlString;
         assets.append(assetSrc);
-        scene.append(letterModel);
-        // playerRightHand.append(letterModel);
+        // scene.append(letterModel);
+        playerRightHand.append(letterModel);
     }
     ajustKerning(str);
 }
